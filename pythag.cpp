5207454a85eb2327f9cd21aeb4dea772b87ee916
Sonny <48072766+SonnyIsMario@users.noreply.github.com>
2021-01-09 22:23:16 -0500

    FIXED YOUR BS CALCULATOR

--- pythag.cpp
+++ pythag.cpp
@@ -1,5 +1,7 @@
+  
 // fuck you jared
 #include "english.h"
+#include <iostream>
 #include <cmath>
 Interger Foremost Left_Parenthesis Right_Parenthesis Left_Bracket
 Double a Semicolon
@@ -10,7 +12,7 @@ std::cout << "enter a leg: " Semicolon
 std::cin >> a Semicolon 
 std::cout << "enter leg two: " Semicolon 
 std::cin >> b Semicolon 
-c equals sqrt Left_Parenthesis pow Left_Parenthesis a,2 Right_Parenthesis + pow Left_Parenthesis b,2 Right_Parenthesis Right_Parenthesis Semicolon 
+c Equals sqrt Left_Parenthesis pow Left_Parenthesis a,2 Right_Parenthesis + pow Left_Parenthesis b,2 Right_Parenthesis Right_Parenthesis Semicolon 
 std::cout << c Semicolon 
 Bring_Back 0 Semicolon 
 Right_Bracket
