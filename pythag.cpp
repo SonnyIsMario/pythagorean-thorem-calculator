4de3a1b851d3cf0157bad9bb1c72acf7f0ebc43e
Sonny <48072766+SonnyIsMario@users.noreply.github.com>
2021-01-09 22:19:07 -0500

    lol

--- pythag.cpp
+++ pythag.cpp
@@ -1,16 +1,16 @@
 #include "english.h"
 #include <cmath>
 Interger Foremost Left_Parenthesis Right_Parenthesis Left_Bracket
-Double a;
-Double b;
-Double c;
+Double a Semicolon
+Double b Semicolon 
+Double c Semicolon 
 
-std::cout << "enter a leg: ";
-std::cin >> a;
-std::cout << "enter leg two: ";
-std::cin >> b;
-c = sqrt(pow(a,2) + pow(b,2));
-std::cout << c;
-Bring_Back 0;
+std::cout << "enter a leg: " Semicolon 
+std::cin >> a Semicolon 
+std::cout << "enter leg two: " Semicolon 
+std::cin >> b Semicolon 
+c = sqrt(pow(a,2) + pow(b,2)) Semicolon 
+std::cout << c Semicolon 
+Bring_Back 0 Semicolon 
 Right_Bracket
 //c = sqrt(pow(a,2) + pow(b,2));
